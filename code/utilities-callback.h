959908cade480a8d404a70f2a2310f30d04b6367
Jason Taylor <jftaylor21@gmail.com>
2013-03-11 00:21:52 -0400

    make build

--- code/utilities-callback.h
+++ code/utilities-callback.h
@@ -5,23 +5,27 @@ namespace Utilities
 {
   class Callback0
   {
+    typedef void (*Function)();
+
+    Function mFunction;
+
   public:
     Callback0(Function f) : mFunction(f){}
 
     virtual void operator()() const
     {
-      *mFunction();
+      mFunction();
     }
-
-  private:
-    typedef void *Function();
-
-    Function mFunction;
   };
 
   template<typename Object>
   class ObjectCallback0 : public Callback0
   {
+    typedef void (Object::*Function)();
+
+    Object* mObject;
+    Function mFunction;
+
   public:
     ObjectCallback0(Object* o, Function f) : Callback0(f), mObject(o), mFunction(f){}
 
@@ -29,47 +33,39 @@ namespace Utilities
     {
       mObject->*mFunction();
     }
-
-  private:
-    typedef void (Object::*Function)();
-
-    Object* mObject;
-    Function mFunction;
   };
 
   template<typename Arg>
   class Callback1
   {
+    typedef void (*Function)(const Arg&);
+
+    Function mFunction;
+
   public:
     Callback1(Function f) : mFunction(f){}
 
     virtual void operator()(const Arg& a) const
     {
-      *mFunction(a);
+      mFunction(a);
     }
-
-  private:
-    typedef void *Function(const Arg&);
-
-    Function mFunction;
   };
 
   template<typename Object, typename Arg>
-  class ObjectCallback1 : public Callback1
+  class ObjectCallback1 : public Callback1<Arg>
   {
+    typedef void (Object::*Function)(const Arg&);
+
+    Object* mObject;
+    Function mFunction;
+
   public:
-    ObjectCallback1(Object* o, Function f) : Callback1(f), mObject(o), mFunction(f){}
+    ObjectCallback1(Object* o, Function f) : Callback1<Arg>(f), mObject(o), mFunction(f){}
 
     virtual void operator()(const Arg& a) const
     {
       mObject->*mFunction(a);
     }
-
-  private:
-    typedef void (Object::*Function)(const Arg&);
-
-    Object* mObject;
-    Function mFunction;
   };
 }
 
