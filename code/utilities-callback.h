0683b582c30f6cad6c944c51a2a37e16b1ce08ac
Jason Taylor <jftaylor21@gmail.com>
2013-04-02 00:52:32 -0400

    add callbacks with 2 and 3 arguments

--- code/utilities-callback.h
+++ code/utilities-callback.h
@@ -90,6 +90,92 @@ namespace Utilities
       (mObject->*mFunction)(a);
     }
   };
+
+  template<typename Arg1, typename Arg2>
+  class Callback2
+  {
+    typedef void (*Function)(const Arg1&, const Arg2&);
+
+    Function mFunction;
+
+  public:
+    Callback2(Function f) : mFunction(f){}
+
+    virtual Callback2* clone() const
+    {
+      return new Callback2<Arg1, Arg2>(*this);
+    }
+
+    virtual void operator()(const Arg1& a1, const Arg2& a2) const
+    {
+      mFunction(a1, a2);
+    }
+  };
+
+  template<typename Object, typename Arg1, typename Arg2>
+  class ObjectCallback2 : public Callback2<Arg1, Arg2>
+  {
+    typedef void (Object::*Function)(const Arg1&, const Arg2&);
+
+    Object* mObject;
+    Function mFunction;
+
+  public:
+    ObjectCallback2(Object* o, Function f) : Callback2<Arg1, Arg2>(nop), mObject(o), mFunction(f){}
+
+    virtual ObjectCallback2* clone() const
+    {
+      return new ObjectCallback2<Object, Arg1, Arg2>(*this);
+    }
+
+    virtual void operator()(const Arg1& a1, const Arg2& a2) const
+    {
+      (mObject->*mFunction)(a1, a2);
+    }
+  };
+
+  template<typename Arg1, typename Arg2, typename Arg3>
+  class Callback3
+  {
+    typedef void (*Function)(const Arg1&, const Arg2&, const Arg3&);
+
+    Function mFunction;
+
+  public:
+    Callback3(Function f) : mFunction(f){}
+
+    virtual Callback3* clone() const
+    {
+      return new Callback3<Arg1, Arg2, Arg3>(*this);
+    }
+
+    virtual void operator()(const Arg1& a1, const Arg2& a2, const Arg3& a3) const
+    {
+      mFunction(a1, a2, a3);
+    }
+  };
+
+  template<typename Object, typename Arg1, typename Arg2, typename Arg3>
+  class ObjectCallback3 : public Callback3<Arg1, Arg2, Arg3>
+  {
+    typedef void (Object::*Function)(const Arg1&, const Arg2&, const Arg3&);
+
+    Object* mObject;
+    Function mFunction;
+
+  public:
+    ObjectCallback3(Object* o, Function f) : Callback3<Arg1, Arg2, Arg3>(nop), mObject(o), mFunction(f){}
+
+    virtual ObjectCallback3* clone() const
+    {
+      return new ObjectCallback3<Object, Arg1, Arg2, Arg3>(*this);
+    }
+
+    virtual void operator()(const Arg1& a1, const Arg2& a2, const Arg3& a3) const
+    {
+      (mObject->*mFunction)(a1, a2, a3);
+    }
+  };
 }
 
 #endif
