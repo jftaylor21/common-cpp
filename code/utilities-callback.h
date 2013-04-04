c78371db3575ab95a8f866492c2664deee170e8e
Jason Taylor <jftaylor21@gmail.com>
2013-04-03 20:41:21 -0400

    make typedef public

--- code/utilities-callback.h
+++ code/utilities-callback.h
@@ -8,11 +8,9 @@ namespace Utilities
 
   class Callback0
   {
+  public:
     typedef void (*Function)();
 
-    Function mFunction;
-
-  public:
     Callback0(Function f) : mFunction(f){}
 
     virtual Callback0* clone() const
@@ -24,17 +22,17 @@ namespace Utilities
     {
       mFunction();
     }
+
+  private:
+    Function mFunction;
   };
 
   template<typename Object>
   class ObjectCallback0 : public Callback0
   {
+  public:
     typedef void (Object::*Function)();
 
-    Object* mObject;
-    Function mFunction;
-
-  public:
     ObjectCallback0(Object* o, Function f) : Callback0(nop), mObject(o), mFunction(f){}
 
     virtual ObjectCallback0* clone() const
@@ -46,16 +44,18 @@ namespace Utilities
     {
       (mObject->*mFunction)();
     }
+
+  private:
+    Object* mObject;
+    Function mFunction;
   };
 
   template<typename Arg>
   class Callback1
   {
+  public:
     typedef void (*Function)(const Arg&);
 
-    Function mFunction;
-
-  public:
     Callback1(Function f) : mFunction(f){}
 
     virtual Callback1* clone() const
@@ -67,17 +67,17 @@ namespace Utilities
     {
       mFunction(a);
     }
+
+  private:
+    Function mFunction;
   };
 
   template<typename Object, typename Arg>
   class ObjectCallback1 : public Callback1<Arg>
   {
+  public:
     typedef void (Object::*Function)(const Arg&);
 
-    Object* mObject;
-    Function mFunction;
-
-  public:
     ObjectCallback1(Object* o, Function f) : Callback1<Arg>(nop), mObject(o), mFunction(f){}
 
     virtual ObjectCallback1* clone() const
@@ -89,16 +89,18 @@ namespace Utilities
     {
       (mObject->*mFunction)(a);
     }
+
+  private:
+    Object* mObject;
+    Function mFunction;
   };
 
   template<typename Arg1, typename Arg2>
   class Callback2
   {
+  public:
     typedef void (*Function)(const Arg1&, const Arg2&);
 
-    Function mFunction;
-
-  public:
     Callback2(Function f) : mFunction(f){}
 
     virtual Callback2* clone() const
@@ -110,17 +112,17 @@ namespace Utilities
     {
       mFunction(a1, a2);
     }
+
+  private:
+    Function mFunction;
   };
 
   template<typename Object, typename Arg1, typename Arg2>
   class ObjectCallback2 : public Callback2<Arg1, Arg2>
   {
+  public:
     typedef void (Object::*Function)(const Arg1&, const Arg2&);
 
-    Object* mObject;
-    Function mFunction;
-
-  public:
     ObjectCallback2(Object* o, Function f) : Callback2<Arg1, Arg2>(nop), mObject(o), mFunction(f){}
 
     virtual ObjectCallback2* clone() const
@@ -132,16 +134,18 @@ namespace Utilities
     {
       (mObject->*mFunction)(a1, a2);
     }
+
+  private:
+    Object* mObject;
+    Function mFunction;
   };
 
   template<typename Arg1, typename Arg2, typename Arg3>
   class Callback3
   {
+  public:
     typedef void (*Function)(const Arg1&, const Arg2&, const Arg3&);
 
-    Function mFunction;
-
-  public:
     Callback3(Function f) : mFunction(f){}
 
     virtual Callback3* clone() const
@@ -153,17 +157,17 @@ namespace Utilities
     {
       mFunction(a1, a2, a3);
     }
+
+  private:
+    Function mFunction;
   };
 
   template<typename Object, typename Arg1, typename Arg2, typename Arg3>
   class ObjectCallback3 : public Callback3<Arg1, Arg2, Arg3>
   {
+  public:
     typedef void (Object::*Function)(const Arg1&, const Arg2&, const Arg3&);
 
-    Object* mObject;
-    Function mFunction;
-
-  public:
     ObjectCallback3(Object* o, Function f) : Callback3<Arg1, Arg2, Arg3>(nop), mObject(o), mFunction(f){}
 
     virtual ObjectCallback3* clone() const
@@ -175,6 +179,10 @@ namespace Utilities
     {
       (mObject->*mFunction)(a1, a2, a3);
     }
+
+  private:
+    Object* mObject;
+    Function mFunction;
   };
 }
 
