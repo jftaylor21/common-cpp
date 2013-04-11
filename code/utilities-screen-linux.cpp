4f482727048ad5f9070c1d580169ed95608154f1
Jason Taylor <jftaylor21@gmail.com>
2013-04-11 09:21:16 -0400

    add ability to set color of console text to linux

--- code/utilities-screen-linux.cpp
+++ code/utilities-screen-linux.cpp
@@ -1,5 +1,6 @@
 #include "utilities-screen-linux.h"
 #include "utilities-screen.h"
+#include <iostream>
 
 bool Utilities::ttyMode(TTYMode mode)
 {
@@ -40,6 +41,45 @@ void Utilities::setTTYMode(TTYMode mode, bool enabled)
     break;
   }
   tcsetattr(STDIN_FILENO, TCSANOW, &t); //set attributes
+}
+
+void Utilities::setTextColor(Color c)
+{
+  int color(0);
+  switch(c)
+  {
+  case COLOR_RESET:
+    color = 0;
+    break;
+  case COLOR_BLACK:
+    color = 30;
+    break;
+  case COLOR_BLUE:
+    color = 34;
+    break;
+  case COLOR_GREEN:
+    color = 32;
+    break;
+  case COLOR_CYAN:
+    color = 36;
+    break;
+  case COLOR_RED:
+    color = 31;
+    break;
+  case COLOR_MAGENTA:
+    color = 35;
+    break;
+  case COLOR_GRAY:
+    color = 37;
+    break;
+  case COLOR_YELLOW:
+    color = 33;
+    break;
+  case COLOR_WHITE:
+    color = 37;
+    break;
+  }
+  std::cout << "\033]" << color << (c==COLOR_GRAY?";2":"") << "m";
 }
 
 void Utilities::echo(bool on)
