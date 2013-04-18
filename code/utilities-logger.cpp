0129e2e13cd7b913caeea972103752331e4792a0
Jason Taylor <jftaylor21@gmail.com>
2013-04-18 01:43:50 -0400

    savepoint

--- code/utilities-logger.cpp
+++ code/utilities-logger.cpp
@@ -1,1 +1,37 @@
 #include "utilities-logger.h"
+#include <cstdio>
+#include <cstdarg>
+
+Utilities::Logger& Utilities::Logger::get()
+{
+  static Logger log;
+  return log;
+}
+
+Utilities::Logger::Logger()
+  : mMaxLogLevel(LOGLEVEL_FATAL)
+{
+}
+
+Utilities::Logger::~Logger()
+{
+}
+
+void Utilities::Logger::setMaxLogLevel(LogLevel level)
+{
+  mMaxLogLevel = level;
+}
+
+Utilities::Logger::LogLevel Utilities::Logger::maxLogLevel()
+{
+  return mMaxLogLevel;
+}
+
+void Utilities::Logger::output(LogLevel level, const char *msg, ...)
+{
+  va_list args;
+  va_start(args, msg);
+  vfprintf(stderr, msg, args);
+  fflush(stderr);
+  va_end(args);
+}
--- code/utilities-logger.h
+++ code/utilities-logger.h
@@ -6,6 +6,32 @@ namespace Utilities
   class Logger
   {
   public:
+    enum LogLevel
+    {
+      LOGLEVEL_FATAL,
+      LOGLEVEL_ERROR,
+      LOGLEVEL_WARNING,
+      LOGLEVEL_INFO
+    };
+
+    static Logger& get();
+    ~Logger();
+
+    void setMaxLogLevel(LogLevel level);
+    LogLevel maxLogLevel();
+
+    void output(LogLevel level, const char* msg, ...);
+    void fatal(const char* msg, ...);
+    void error(const char* msg, ...);
+    void warning(const char* msg, ...);
+    void info(const char* msg, ...);
+
+  private:
+    Logger();
+    Logger(const Logger&);
+    Logger& operator=(const Logger&);
+
+    LogLevel mMaxLogLevel;
   };
 }
 
